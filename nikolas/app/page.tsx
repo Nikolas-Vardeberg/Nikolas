9e679e0f6a9862eb5179f9fe489769d4ea77c897
niklusrook <post@nikolas.no>
2024-02-19 10:31:36 +0100

    next app

---
+++ nikolas/app/page.tsx
@@ -0,0 +1,9 @@
+
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen items-center justify-center">
+      <h1 className="font-semibold text-5xl">Nikolas Vardeneger Rekeskeiv</h1>
+    </main>
+  );
+}
